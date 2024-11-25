a72eb771b262f41c613e59317bcc82e59b79e702
IgorBrizola <igorbrizzola@gmail.com>
2024-11-25 19:55:12 -0300

    chore: remove imports sem ultilizacao

--- src/main/kotlin/com/auth/jwt/repository/ArticleRepository.kt
+++ src/main/kotlin/com/auth/jwt/repository/ArticleRepository.kt
@@ -3,7 +3,6 @@ package com.auth.jwt.repository
 import com.auth.jwt.model.Article
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import java.util.UUID
 
 @Repository
 interface ArticleRepository : JpaRepository<Article, String>
